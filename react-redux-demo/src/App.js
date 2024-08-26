9d94c2e8c47f87e5bdc2adcf9ecdce60ec628732
Hrishikesh Bade <hrishikeshbade007@gmail.com>
2024-08-26 17:17:19 +0530

    using to update the state using hooks for redux like useDispatch and useSelector

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Provider} from 'react-redux'
 import store from './redux/store'
-import CakeContainer from './components/CakeContainer';
+// import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
-      <CakeContainer></CakeContainer>
+      <HooksCakeContainer></HooksCakeContainer>
+      {/* <CakeContainer></CakeContainer> */}
     </div>
     </Provider>
   );
