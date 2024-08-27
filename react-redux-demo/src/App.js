72f75ac1b03fd267b49871c8c20a6314bb5006de
Hrishikesh Bade <hrishikeshbade007@gmail.com>
2024-08-27 12:22:59 +0530

    icecream redux

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import {Provider} from 'react-redux'
 import store from './redux/store'
 // import CakeContainer from './components/CakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
+import HooksIceCreamContainer from './components/HooksIceCream';
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
       <HooksCakeContainer></HooksCakeContainer>
+      <HooksIceCreamContainer></HooksIceCreamContainer>
+      {/* <IceCreamContainer></IceCreamContainer> */}
       {/* <CakeContainer></CakeContainer> */}
     </div>
     </Provider>
--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,9 +1,10 @@
 // redux store (state)
 
 import {createStore} from 'redux'
-import cakeReducer from './cake/cakeReducer'
+import rootReducer from './rootReducer'
 
-const store = createStore(cakeReducer)
+
+const store = createStore(rootReducer)
 
 
 export default store;
