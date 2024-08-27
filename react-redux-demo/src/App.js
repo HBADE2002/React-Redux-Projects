9636049ce2871f2936982195a23e18502625f8fb
Hrishikesh Bade <hrishikeshbade007@gmail.com>
2024-08-27 17:35:18 +0530

    icecream update

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {Provider} from 'react-redux'
 import store from './redux/store'
 // import CakeContainer from './components/CakeContainer';
-import IceCreamContainer from './components/IceCreamContainer';
+// import IceCreamContainer from './components/IceCreamContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import HooksIceCreamContainer from './components/HooksIceCream';
 function App() {
